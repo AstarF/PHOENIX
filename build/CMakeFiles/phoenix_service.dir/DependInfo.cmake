647d74d9982b1c8eda645837d84da23184422ffa
AstarF <AstarFancy@outlook.com>
2022-06-21 21:25:58 +0800

    init

---
+++ build/CMakeFiles/phoenix_service.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/phoenix/src/main.cpp" "/home/ubuntu/phoenix/build/CMakeFiles/phoenix_service.dir/src/main.cpp.o"
+  "/home/ubuntu/phoenix/src/px_callback_hub.cpp" "/home/ubuntu/phoenix/build/CMakeFiles/phoenix_service.dir/src/px_callback_hub.cpp.o"
+  "/home/ubuntu/phoenix/src/px_connections.cpp" "/home/ubuntu/phoenix/build/CMakeFiles/phoenix_service.dir/src/px_connections.cpp.o"
+  "/home/ubuntu/phoenix/src/px_epoll.cpp" "/home/ubuntu/phoenix/build/CMakeFiles/phoenix_service.dir/src/px_epoll.cpp.o"
+  "/home/ubuntu/phoenix/src/px_events.cpp" "/home/ubuntu/phoenix/build/CMakeFiles/phoenix_service.dir/src/px_events.cpp.o"
+  "/home/ubuntu/phoenix/src/px_http.cpp" "/home/ubuntu/phoenix/build/CMakeFiles/phoenix_service.dir/src/px_http.cpp.o"
+  "/home/ubuntu/phoenix/src/px_http_server.cpp" "/home/ubuntu/phoenix/build/CMakeFiles/phoenix_service.dir/src/px_http_server.cpp.o"
+  "/home/ubuntu/phoenix/src/px_locker.cpp" "/home/ubuntu/phoenix/build/CMakeFiles/phoenix_service.dir/src/px_locker.cpp.o"
+  "/home/ubuntu/phoenix/src/px_module.cpp" "/home/ubuntu/phoenix/build/CMakeFiles/phoenix_service.dir/src/px_module.cpp.o"
+  "/home/ubuntu/phoenix/src/px_mysql.cpp" "/home/ubuntu/phoenix/build/CMakeFiles/phoenix_service.dir/src/px_mysql.cpp.o"
+  "/home/ubuntu/phoenix/src/px_resources.cpp" "/home/ubuntu/phoenix/build/CMakeFiles/phoenix_service.dir/src/px_resources.cpp.o"
+  "/home/ubuntu/phoenix/src/px_server.cpp" "/home/ubuntu/phoenix/build/CMakeFiles/phoenix_service.dir/src/px_server.cpp.o"
+  "/home/ubuntu/phoenix/src/px_signal.cpp" "/home/ubuntu/phoenix/build/CMakeFiles/phoenix_service.dir/src/px_signal.cpp.o"
+  "/home/ubuntu/phoenix/src/px_thread_pool.cpp" "/home/ubuntu/phoenix/build/CMakeFiles/phoenix_service.dir/src/px_thread_pool.cpp.o"
+  "/home/ubuntu/phoenix/src/px_timer.cpp" "/home/ubuntu/phoenix/build/CMakeFiles/phoenix_service.dir/src/px_timer.cpp.o"
+  "/home/ubuntu/phoenix/src/px_util.cpp" "/home/ubuntu/phoenix/build/CMakeFiles/phoenix_service.dir/src/px_util.cpp.o"
+  "/home/ubuntu/phoenix/src/px_worker.cpp" "/home/ubuntu/phoenix/build/CMakeFiles/phoenix_service.dir/src/px_worker.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/phoenix_service.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/phoenix_service.dir/src/main.cpp.o"
+  "CMakeFiles/phoenix_service.dir/src/px_callback_hub.cpp.o"
+  "CMakeFiles/phoenix_service.dir/src/px_connections.cpp.o"
+  "CMakeFiles/phoenix_service.dir/src/px_epoll.cpp.o"
+  "CMakeFiles/phoenix_service.dir/src/px_events.cpp.o"
+  "CMakeFiles/phoenix_service.dir/src/px_http.cpp.o"
+  "CMakeFiles/phoenix_service.dir/src/px_http_server.cpp.o"
+  "CMakeFiles/phoenix_service.dir/src/px_locker.cpp.o"
+  "CMakeFiles/phoenix_service.dir/src/px_module.cpp.o"
+  "CMakeFiles/phoenix_service.dir/src/px_mysql.cpp.o"
+  "CMakeFiles/phoenix_service.dir/src/px_resources.cpp.o"
+  "CMakeFiles/phoenix_service.dir/src/px_server.cpp.o"
+  "CMakeFiles/phoenix_service.dir/src/px_signal.cpp.o"
+  "CMakeFiles/phoenix_service.dir/src/px_thread_pool.cpp.o"
+  "CMakeFiles/phoenix_service.dir/src/px_timer.cpp.o"
+  "CMakeFiles/phoenix_service.dir/src/px_util.cpp.o"
+  "CMakeFiles/phoenix_service.dir/src/px_worker.cpp.o"
+  "../bin/phoenix_service.pdb"
+  "../bin/phoenix_service"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/phoenix_service.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
